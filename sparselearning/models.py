344100c333484cc0c5a5a27e2b06739de577bbe9
andy li <lihanou@yahoo.com>
2024-09-04 17:42:11 +0100

    weight sharing 3

--- sparselearning/models.py
+++ sparselearning/models.py
@@ -683,8 +683,8 @@ def ResNet18(c=1000):
 
 # def ResNet34(c=10):
 #     return ResNet(BasicBlock, [3,4,6,3],c)
-def ResNet34(num_classes=10, ratio=2):
-    return ResNet([BasicBlock, BasicBlock_NoPara], [3, 4, 6, 3], num_classes=num_classes, ratio=ratio)
+def ResNet34(c=10, ratio=2):
+    return ResNet([BasicBlock, BasicBlock_NoPara], [3, 4, 6, 3], num_classes=c, ratio=ratio)
 
 
 def ResNet50(c=10):
