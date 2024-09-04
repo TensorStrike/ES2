5186bc8d118b227c96578f56d597dd28dfad7de6
andy li <lihanou@yahoo.com>
2024-09-04 18:20:56 +0100

    weight sharing 3

--- sparselearning/models.py
+++ sparselearning/models.py
@@ -6,7 +6,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from sparselearning.dyrelu import DyReLUB
-from initializers import initializations
 
 
 class SparseSpeedupBench(object):
